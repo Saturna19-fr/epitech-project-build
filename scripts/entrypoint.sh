4c671f8dcef593e30f9d97344ba8ad7f9c51cfa1
Thibault - TheEternalSaturn <thibault.morez@outlook.com>
2026-05-31 23:47:04 +0200

    Remove echo statements from entrypoint script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-echo "=== START ==="
+
 make re 2>&1 | tee /tmp/make_output.txt
-echo "=== MAKE DONE ==="
+
 python3 /scripts/coding_style_checker.py < /tmp/make_output.txt
-echo "=== PYTHON DONE ==="
